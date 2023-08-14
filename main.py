e6859695d78c432e8d7a9ec2f4b19213f4a48fb0
Icode-python <me.fast.learn@gmail.com>
2023-08-14 12:25:15 -0400

    created player class and refined player movement

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
 import pygame
 import sys
-
-def player_animation():
-    
-    player.x += player_x_speed
-    player.y += player_y_speed
-    if player.left <= 0:
-        player.left = 0
-    if player.right >= screen_width:
-        player.right = screen_width
-    if player.top <= 0:
-        player.top = 0
-    if player.bottom >= screen_height:
-        player.bottom = screen_height
+from player import *
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -24,35 +12,15 @@ pygame.display.set_caption('rpg_game')
 
 bg_color = pygame.Color('grey12')
 light_grey = (200, 200, 200)
-player_x_speed = 0
-player_y_speed = 0
-player = pygame.Rect(screen_width / 2 - 50, screen_height / 2 - 50, 100, 100)
+player = Player(screen_width / 2 - 50, screen_height / 2 - 50, 100, 100)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:
-                player_y_speed = -7
-            if event.key == pygame.K_s:
-                player_y_speed = 7
-            if event.key == pygame.K_d:
-                player_x_speed = 7
-            if event.key == pygame.K_a:
-                player_x_speed = -7
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_w:
-                player_y_speed = 0
-            if event.key == pygame.K_s:
-                player_y_speed = 0
-            if event.key == pygame.K_d:
-                player_x_speed = 0
-            if event.key == pygame.K_a:
-                player_x_speed = 0
 
-    player_animation()
+    player.move(screen_width, screen_height)
 
     screen.fill(bg_color)
     pygame.draw.rect(screen, light_grey, player)
---
+++ player.py
@@ -0,0 +1,40 @@
+import pygame
+import sys
+
+
+class Player():
+
+    def __init__(self, x, y, width, height, speed=7):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.rect = pygame.Rect(x, y, width, height)
+        self.speed = speed
+
+    def move(self, screen_width, screen_height):
+        
+        keys=pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.y -= self.speed
+        if keys[pygame.K_s]:
+            self.y += self.speed
+        if keys[pygame.K_d]:
+            self.x += self.speed
+        if keys[pygame.K_a]:
+            self.x -= self.speed
+        
+        self.rect.x = self.x
+        self.rect.y = self.y
+        
+        if self.rect.left <= 0:
+            self.rect.left = 0
+        if self.rect.right >= screen_width:
+            self.rect.right = screen_width
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= screen_height:
+            self.rect.bottom = screen_height
+
+        self.x = self.rect.x
+        self.y = self.rect.y
